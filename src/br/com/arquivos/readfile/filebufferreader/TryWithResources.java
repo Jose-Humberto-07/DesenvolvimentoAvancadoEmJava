6f13c84330e74abe8202999b9269ca00b786a22f
José Humberto <jhpnascimento96@gmail.com>
2022-04-24 22:22:22 -0300

    File manipulation

---
+++ src/br/com/arquivos/readfile/filebufferreader/TryWithResources.java
@@ -0,0 +1,24 @@
+package br.com.arquivos.readfile.filebufferreader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+public static void main(String[] args) {
+		
+		String path = "C:\\Windows\\Temp\\in.txt";
+		
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} 
+	}
+}
